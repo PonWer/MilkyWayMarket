ae8571dfe33aeb7dd5a2841e3ae2e3e67a4119ae
Pontus Werme <pontus@werme.org>
2023-09-29 22:08:46 +0200

    pages

--- MilkyWayMarket/DataService.cs
+++ MilkyWayMarket/DataService.cs
@@ -8,15 +8,27 @@ namespace MilkyWayMarket
 		Task Init();
 
 		List<Container> DataPoints { get; }
+		int EmptyDataPoints { get; }
+		int DataPointsToFetch { get; }
 
 		bool Loading { get; }
 		MudTheme CurrentTheme { get; set; }
+
+		public Dictionary<string, ItemHistory> History { get; }
+
+		public List<string> HistoryKeys { get; }
+		event EventHandler DataUpdated;
 	}
 	public class DataService : IDataService
 	{
 		private bool initiated = false;
 		private List<Container> dataPoints = new List<Container>();
+		private List<Container> emptyDataPoints = new List<Container>();
 		private MudTheme _currentTheme;
+		private Dictionary<string, ItemHistory> history = new Dictionary<string, ItemHistory>();
+		private List<string> historyKeys = new List<string>();
+
+		public event EventHandler DataUpdated;
 
 		public bool Loading => !initiated;
 
@@ -27,11 +39,45 @@ namespace MilkyWayMarket
 		}
 
 		public List<Container> DataPoints => dataPoints;
+		public int EmptyDataPoints => emptyDataPoints.Count;
+		public int DataPointsToFetch => 120;
+
+		public Dictionary<string, ItemHistory> History => history;
+
+		public List<string> HistoryKeys => historyKeys;
 
 		public async Task Init()
 		{
-			dataPoints = await GetCommitData.Call(await GetDeployments.Call());
-			initiated = true;
+			if(initiated)
+				return;
+
+			emptyDataPoints = await GetDeployments.Call(DataPointsToFetch);
+			foreach (var emptyDataPoint in emptyDataPoints)
+			{
+				dataPoints.Insert(0,await GetCommitData.Call(emptyDataPoint));
+				DataUpdated.Invoke(null,EventArgs.Empty);
+			}
+			
+            foreach (var dataPoint in dataPoints)
+            {
+                foreach (var item in dataPoint.Items)
+                {
+	                history.TryAdd(item.Name, new ItemHistory());
+
+	                if(dataPoint.Date != null && !history[item.Name].history.ContainsKey((DateTime)dataPoint.Date))
+					    history[item.Name].history.Add((DateTime)dataPoint.Date,item);
+                }
+            }
+
+            historyKeys = history.Keys.ToList();
+
+            initiated = true;
+            DataUpdated.Invoke(null, EventArgs.Empty);
 		}
 	}
+
+    public class ItemHistory
+    {
+       public Dictionary<DateTime,Item> history = new Dictionary<DateTime,Item>();
+    }
 }
