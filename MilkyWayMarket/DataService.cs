df92cb56adbafade6e74b26351a25715a4041c05
Pontus Werme <pontus@werme.org>
2023-09-29 19:57:39 +0200

    Test

--- MilkyWayMarket/DataService.cs
+++ MilkyWayMarket/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using MilkyWayMarket.Code;
+using MudBlazor;
 
 namespace MilkyWayMarket
 {
@@ -9,13 +10,22 @@ namespace MilkyWayMarket
 		List<Container> DataPoints { get; }
 
 		bool Loading { get; }
+		MudTheme CurrentTheme { get; set; }
 	}
 	public class DataService : IDataService
 	{
 		private bool initiated = false;
 		private List<Container> dataPoints = new List<Container>();
+		private MudTheme _currentTheme;
 
 		public bool Loading => !initiated;
+
+		public MudTheme CurrentTheme
+		{
+			get => _currentTheme;
+			set => _currentTheme = value;
+		}
+
 		public List<Container> DataPoints => dataPoints;
 
 		public async Task Init()
